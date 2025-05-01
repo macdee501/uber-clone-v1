28ca6cb18b485744551a8f70b41a660bfa065843
mcdonald <mcdonaldlekhoaba@gmail.com>
2025-05-01 16:53:29 +0200

    first commit

--- components/mainSection/ActionButton.tsx
+++ components/mainSection/ActionButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function ActionButton({ value,icon }) {
+export default function ActionButton({ value }) {
   return (
     <>
     <div className="bg-gray-200 flex  flex-col flex-1 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl">
